950d5bcfa64febdd7bf47f47862563d3902cc525
NemchenkoNazar <97nazar.97@gmail.com>
2021-02-23 12:36:25 +0200

    Исправил

--- HW2/main.js
+++ HW2/main.js
@@ -1,15 +1,15 @@
-let firstNumber = parseInt(prompt("Введите первое число: "));
-while (firstNumber % 1 !== 0){
-    firstNumber = parseInt(prompt("Неверно! Вам нужно ввести целое число: "));
+let firstNumber = +prompt("Введите первое число: ");
+while (!Number.isInteger(firstNumber)){
+    firstNumber = +prompt("Неверно! Вам нужно ввести целое число: ");
 }
 
-let secondNumber = parseInt(prompt("Введите второе число: "));
-while (secondNumber % 1 !== 0){
-    secondNumber = parseInt(prompt("Неверно! Вам нужно ввести целое число: "));
+let secondNumber = +prompt("Введите второе число: ");
+while (!Number.isInteger(secondNumber)){
+    secondNumber = +prompt("Неверно! Вам нужно ввести целое число: ");
 }
 
 while ( firstNumber >= secondNumber){
-    secondNumber = parseInt(prompt("Второе число должно быть больше первого: "));
+    secondNumber = +prompt("Второе число должно быть больше первого: ");
 }
 
 const withEven = confirm("Учитывать четные числа?");
