6184c79fa5f89372fe03b4ff3369ee2e47fa2a8e
NemchenkoNazar <97nazar.97@gmail.com>
2021-03-02 00:37:34 +0200

    Done!

---
+++ HW5/main.js
@@ -0,0 +1,26 @@
+function getRandomArray(length, min, max){
+    let randomArray = [];
+    for (let i = 0; i < length; i++) {
+        let randomNumber = 0;
+        while(randomNumber < min) {
+            randomNumber = Math.floor(Math.random() * max) + 1;
+        }
+        randomArray[i] = randomNumber;
+
+    }
+    return randomArray;
+}
+// console.log(`Функция №1 : ${getRandomArray(15, 90, 100)}`);
+
+// function getModa(...numbers){
+//     let count = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         const number = numbers[i];
+//         if (number % 1 === 0){
+//
+//         }
+//     }
+//     return ;
+// }
+// const arr = [1,2,3];
+// console.log(getModa(...arr));
